7925b576ece20534b90dcd4f8c36afc8486b1fd0
Michael Abshoff <michael.abshoff@gmail.com>
2007-12-31 23:13:49 +0100

    Change link order to satisfy the Cygwin linker

---
+++ qcurves/tdivpol.cc
@@ -0,0 +1,54 @@
+// tdivpol.cc -- test for division poly functions in divpol.h/cc 
+//////////////////////////////////////////////////////////////////////////
+//
+// Copyright 1990-2005 John Cremona
+// 
+// This file is part of the mwrank package.
+// 
+// mwrank is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+// 
+// mwrank is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with mwrank; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+//////////////////////////////////////////////////////////////////////////
+//
+
+#include "curve.h"
+
+#include "polys.h"
+#include "divpol.h"
+
+int main()
+{
+  //  set_precision(string("Enter number of decimal places").c_str());
+  initprimes(string("PRIMES").c_str(),0);
+
+  Curve E(BIGINT(0),BIGINT(0),BIGINT(1),BIGINT(-7),BIGINT(6));
+
+  Curvedata C(E);
+  cout << "Curve " << E << endl;
+
+  cout<<"Division Poly (2) = \t"<<makepdivpol(&C,2)<<endl;
+  int i;
+  for(i=3; i<12; i+=2)
+    cout<<"Division Poly ("<<i<<") = \t"<<makepdivpol(&C,i)<<endl;
+
+
+}
+
+
+//end of file tdivpol.cc
+
+
+
+
+
---
+++ qcurves/twist.cc
@@ -0,0 +1,73 @@
+// twist.cc: program to compute & display quadratic twists
+//////////////////////////////////////////////////////////////////////////
+//
+// Copyright 1990-2005 John Cremona
+// 
+// This file is part of the mwrank package.
+// 
+// mwrank is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+// 
+// mwrank is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with mwrank; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+//////////////////////////////////////////////////////////////////////////
+ 
+#include "curve.h"
+#include "getcurve.h"
+
+int main(){
+
+  initprimes(string("PRIMES").c_str(),0);
+
+  Curvedata D, E;
+  CurveRed CR;
+  bigint c4,c6,twist2;
+  bigint v;
+  int verbose=0;
+  vector<bigrational> ai(5);
+
+
+while (getcurve(ai,verbose))
+{
+  Curvedata D(Curvedata(ai,v),1);
+  E = D;
+  CR = CurveRed(E);
+  cout << "\nCurve is:  " << endl;  CR.display(cout);
+  
+  long twist=1;
+  
+  while(1)
+    {
+      cout << "\nEnter a twist value: ";
+      cout << "\n(0 to set a new `original' curve, 1 to twist original,\n";
+      cout << "or any other integer to twist immediately preceding output)\n ";
+      cin>>ws;  if(cin.eof()) {cout<<endl; exit(0);}
+      cin >> twist;
+      if(twist==0) break;
+      if(twist==1) { E=D;}
+      else
+	{
+	  E.getci(c4, c6);
+	  twist *= 4;        //corrects for twist not congruent -1 mod 4
+	  twist2 = twist*twist;
+	  c4 *= twist2;
+	  c6 *= twist*twist2;
+	  E = Curvedata(Curve(c4, c6), 1); //minimal form
+	  CR=CurveRed(E);
+	  cout << "\n\nE * " << twist/4 << " is:  \n"; CR.display(cout); 
+	  cout << endl;
+	}
+    }
+}
+
+
+} //ends main
